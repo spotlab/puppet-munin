6c386bee0cb861b448e16049c0bb0dc5601078da
Léo POIROUX <leo.poiroux@gmail.com>
2014-06-03 17:38:11 +0200

    Disable export because no storeconfigs

--- manifests/init.pp
+++ manifests/init.pp
@@ -523,15 +523,15 @@ class munin (
   }
 
   # Exported Resource for Server automatic configuration
-  @@file { "${munin::include_dir}/${::fqdn}.conf":
-    ensure  => $munin::manage_file,
-    path    => "${munin::include_dir}/${::fqdn}.conf",
-    mode    => $munin::config_file_mode,
-    owner   => $munin::config_file_owner,
-    group   => $munin::config_file_group,
-    content => template($munin::template_host),
-    tag     => "munin_host_${munin::magic_tag}",
-  }
+  # @@file { "${munin::include_dir}/${::fqdn}.conf":
+  #   ensure  => $munin::manage_file,
+  #   path    => "${munin::include_dir}/${::fqdn}.conf",
+  #   mode    => $munin::config_file_mode,
+  #   owner   => $munin::config_file_owner,
+  #   group   => $munin::config_file_group,
+  #   content => template($munin::template_host),
+  #   tag     => "munin_host_${munin::magic_tag}",
+  # }
 
 
   ### Include custom class if $my_class is set
